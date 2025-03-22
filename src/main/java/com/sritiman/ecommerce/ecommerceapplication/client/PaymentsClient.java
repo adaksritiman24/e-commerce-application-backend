2e4d0975a562a8f756edbf60e0004d586720484f
Sritiman Adak <adaksritiman24@gmail.com>
2025-03-22 15:29:56 +0530

    Add resilience4j retry for Payments Client

--- src/main/java/com/sritiman/ecommerce/ecommerceapplication/client/PaymentsClient.java
+++ src/main/java/com/sritiman/ecommerce/ecommerceapplication/client/PaymentsClient.java
@@ -3,14 +3,21 @@ package com.sritiman.ecommerce.ecommerceapplication.client;
 import com.sritiman.ecommerce.ecommerceapplication.client.configs.PaymentsConfiguration;
 import com.sritiman.ecommerce.ecommerceapplication.model.payments.PaymentAuthorizationRequest;
 import com.sritiman.ecommerce.ecommerceapplication.model.payments.PaymentResponseDTO;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import static com.sritiman.ecommerce.ecommerceapplication.constants.Constants.PAYMENTS_CLIENT;
 import static com.sritiman.ecommerce.ecommerceapplication.constants.Constants.PAYMENTS_SERVICE;
 
 @FeignClient(name = PAYMENTS_SERVICE, url = "${feign.client.config.payment-service.url}", configuration = PaymentsConfiguration.class)
 public interface PaymentsClient {
     @PostMapping("/payments/v1/capture")
+    @Retry(name = PAYMENTS_CLIENT, fallbackMethod = "authorizeFallback")
     PaymentResponseDTO authorizeCapture(@RequestBody PaymentAuthorizationRequest paymentAuthorizationRequest);
+
+    default PaymentResponseDTO authorizeFallback(RuntimeException ex) {
+        throw ex;
+    }
 }
--- src/main/java/com/sritiman/ecommerce/ecommerceapplication/constants/Constants.java
+++ src/main/java/com/sritiman/ecommerce/ecommerceapplication/constants/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String PAYMENT_MODE_NOT_SUPPORTED = "PaymentMode not Supported";
     public static final String ACCOUNT_NOT_FOUND = "Account not found";
     public static final String ACCOUNT_BALANCE_INSUFFICIENT = "Account Balance Is Insufficient";
+    public static final String PAYMENTS_CLIENT = "Payments-Client";
 }
